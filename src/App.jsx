dceed11d79759fc9f7a098b4c5993ffdf0858297
ykulkarni <ykulkarni@integralads.com>
2023-05-21 09:31:07 +0530

    CodeFest project UI

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import BarChart from './components/BarChart';
+
+function App() {
+  return (
+    <div className="App">
+      <BarChart />
+    </div>
+  );
+}
+
+export default App;
